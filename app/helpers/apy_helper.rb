267e4bbb57e2add05699c11000afd721e1729587
danigb <danigb@gmail.com>
2010-09-23 15:48:35 +0200

    better admin navigation. better menus

--- app/helpers/apy_helper.rb
+++ app/helpers/apy_helper.rb
@@ -5,8 +5,13 @@ module ApyHelper
   end
 
   def show_childs?(current_page, current_item)
-    current_item.depth < 3 || (current_page == current_item && current_item.depth == 3 && current_item.children.size > 0 ) || current_page.parent == current_item
+    should_show_childs?(3, current_page, current_item)
   end
+
+ def should_show_childs?(depth, current_page, current_item)
+    current_item.depth < depth || (current_page == current_item && current_item.depth == depth && current_item.children.size > 0 ) || current_page.parent == current_item
+  end
+
   
   def apy_tabs(current)
     tabs do 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_apyweb_session',
-  :secret      => '31b47148ff0f8d87a34935c3b78788d5a3ede3efa3db74a56eec9a06e0f5b44adcde5525d5a183e98fad1a4fca435f3833ed5f65c6543dbba70afb23c482ee27'
+  :key         => '_pagify_session',
+  :secret      => '77e229f82ed416027ca0e87a222bee40be17298365d0967ad9639f904ac0444228bbead70c772239674c0332897a393c479530a9930d155c3c13060fa2df29fd'
 }
 
 # Use the database for sessions instead of the cookie-based default,
